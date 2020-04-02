3f6ab8b9bf68bb2b116b24377ff5f6180cda5d2e
Teddy Ortega <tortega128@gmail.com>
2020-04-02 10:25:29 -0700

    remove annoying mobile zoom?

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import 'antd/dist/antd.css';
 import 'styles/main.scss';
 
@@ -7,5 +8,13 @@ type AppPropsType = {
 };
 
 export default function MyApp({ Component, pageProps }: AppPropsType) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Animal Crossing Helper</title>
+        <meta name="viewport" content="width=device-width, user-scalable=no" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
